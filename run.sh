88fc1e8d93e12e8ab19c4c7dd4d5dd7708019684
Peter Friedland <peter@mansfield-analytics.com>
2023-05-29 18:31:50 -0400

    moving to na-market as top level directory

--- run.sh
+++
@@ -1,7 +0,0 @@
-#export ENERGY_MATEO_USER=
-#export ENERGY_MATEO_PWD=
-export SOURCE_FILE="/Users/pfriedland/Documents/projects/enel/na-markets/aeso"
-export OPENSSL_CONF=$SOURCE_FILE/openssl.cnf
-export NA_MARKETS_AESO_CONFIG_FILE=$SOURCE_FILE/config.json
-#export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
-bash -c "source $SOURCE_FILE/.pyenv/bin/activate; python3 $SOURCE_FILE/main.py" 
